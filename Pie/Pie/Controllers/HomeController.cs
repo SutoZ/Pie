1794ae2d2a434697e87b51d036038658b5114f80
SutoZ <suto.zoli92@gmail.com>
2019-03-30 22:02:03 +0100

    Add files via upload

---
+++ Pie/Pie/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            //constructor injection
+            _pieRepository = pieRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
