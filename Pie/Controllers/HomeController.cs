69936dd330070763d3fc9993a6e7e601da2034a4
Suto Zoltan <suto.zoli92@gmail.com>
2019-03-30 22:26:47 +0100

    HomeController with view added

---
+++ Pie/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            //constructor injection
+            _pieRepository = pieRepository;
+        }
+        public IActionResult Index()
+        {
+            //return View(_pieRepository.);
+            var pies = _pieRepository
+                        .GetAllPies()
+                        .OrderBy(x => x.Name);
+
+            return View(pies);
+        }
+    }
+}
